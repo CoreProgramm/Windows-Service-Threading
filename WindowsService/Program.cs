61a1935fb919f982c28b746ee81a410cd6a1674d
JT <coreprogramm@gmail.com>
2019-08-18 01:02:58 +0530

    Windows Service-Threading
    In this CoreProgramm you find how to create and Install Windows Service using Threading class.

---
+++ WindowsService/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsService
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+            //Service1 service = new Service1();
+            //service.onDebug();
+
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new Service1()
+            };
+            ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
---
+++ WindowsService/Service1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.IO;
+
+namespace WindowsService
+{
+    public partial class Service1 : ServiceBase
+    {
+        int ScheduleInterval = Convert.ToInt32(ConfigurationSettings.AppSettings["ThreadSleepTimeInMin"]);
+        public Thread worker = null;
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            try
+            {
+                ThreadStart start = new ThreadStart(Working);
+                worker = new Thread(start);
+                worker.Start();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        public void Working()
+        {
+            while (true)
+            {
+
+                string path = "C:\\Sample.txt";
+                using (StreamWriter writer = new StreamWriter(path, true))
+                {
+                    writer.WriteLine(string.Format("Windows Service Called on " + DateTime.Now.ToString("dd /MM/yyyy hh:mm:ss tt")));
+                    writer.Close();
+                }
+                Thread.Sleep(ScheduleInterval * 60 * 1000);
+            }
+        }
+        public void onDebug()
+        {
+            OnStart(null);
+        }
+        protected override void OnStop()
+        {
+            try
+            {
+                if ((worker != null) & worker.IsAlive)
+                {
+                    worker.Abort();
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        
+    }
+}
